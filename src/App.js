0f8c54b6a925acc9ee0d27d79fe245dcb06c1838
deafsloth <deafsloth@gmail.com>
2022-07-06 15:26:40 +0900

    git init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import React from "react";
+import RestAPI from "./RestAPI.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <RestAPI />
+      </header>
+    </div>
+  );
+}
+
+export default App;
